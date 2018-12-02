a0f4a93207ebf1c374543bc29265a044c128aeb7
Lindsay Elliott <lellio04@uoguelph.ca>
2018-12-02 11:03:09 -0500

    some controller logic for user_challenges_status and seeds for db

--- app/controllers/api/v1/user_challenges_status_controller.rb
+++ app/controllers/api/v1/user_challenges_status_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::UserChallengesStatusController < ApplicationController
+  before_action :doorkeeper_authorize!  #, except: [:index, :show]
+  before_action :set_user_challenges_status, only: [:destroy]
+
+  # GET /user_challenges_status
+  def index
+    @user_challenges_statuses = UserChallengesStatus.all
+    json_response(@user_challenges_statuses)
+  end
+
   def show
   end
 
@@ -6,5 +15,17 @@ class Api::V1::UserChallengesStatusController < ApplicationController
   end
 
   def update
+  end
+
+  # DELETE /user_challenges_status/:id
+  def destroy
+    @user_challenges_status.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_user_challenges_status
+    @user_challenges_status = UserChallengesStatus.find(params[:id])
   end
 end
