ae6a6a5cf1ef97d27164241359a04b87802c2517
Anthony Robertson <arobertson1796@gmail.com>
2019-02-24 02:10:01 -0500

    refining serializers to actually serialize the correct data

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -1,3 +1,3 @@
 class ChallengeSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :description, :start_date, :end_date
 end
--- app/serializers/user_challenge_serializer.rb
+++ app/serializers/user_challenge_serializer.rb
@@ -1,4 +1,4 @@
 class UserChallengeSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :challenge_id, :user_id, :status_id, :accept_date,            :created_at, :updated_at
   belongs_to :challenge
 end
